a5f4ec4faec697c1c065d9dc2f69887dcbcfaebe
David Diaz <sinedindnd@hotmail.com>
2025-06-14 13:40:55 -0500

    CRUD FormulariosDepartamentos

--- Formularios/frmDepartamento.Designer.vb
+++ Formularios/frmDepartamento.Designer.vb
@@ -42,12 +42,12 @@ Partial Class frmDepartamento
         Me.Label6 = New System.Windows.Forms.Label()
         Me.txtBuscar = New System.Windows.Forms.TextBox()
         Me.dgvDepartamento = New System.Windows.Forms.DataGridView()
+        Me.btnBuscar = New System.Windows.Forms.Button()
         Me.id_departamento = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.nombre_area_departamento = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.descripcion = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.centro_de_costos = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.nombre_jefe_area = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.btnBuscar = New System.Windows.Forms.Button()
         Me.Panel1.SuspendLayout()
         CType(Me.dgvDepartamento, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -55,75 +55,85 @@ Partial Class frmDepartamento
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(15, 33)
+        Me.Label1.Location = New System.Drawing.Point(11, 27)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(93, 16)
+        Me.Label1.Size = New System.Drawing.Size(74, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Departamento"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(425, 33)
+        Me.Label2.Location = New System.Drawing.Point(404, 27)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(108, 16)
+        Me.Label2.Size = New System.Drawing.Size(87, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Centro de costos"
         '
         'txtDepartamento
         '
-        Me.txtDepartamento.Location = New System.Drawing.Point(133, 27)
+        Me.txtDepartamento.Location = New System.Drawing.Point(102, 19)
+        Me.txtDepartamento.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.txtDepartamento.Name = "txtDepartamento"
-        Me.txtDepartamento.Size = New System.Drawing.Size(271, 22)
+        Me.txtDepartamento.Size = New System.Drawing.Size(301, 20)
         Me.txtDepartamento.TabIndex = 2
         '
         'txtCentroCostos
         '
-        Me.txtCentroCostos.Location = New System.Drawing.Point(546, 27)
+        Me.txtCentroCostos.Location = New System.Drawing.Point(492, 22)
+        Me.txtCentroCostos.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.txtCentroCostos.Multiline = True
         Me.txtCentroCostos.Name = "txtCentroCostos"
-        Me.txtCentroCostos.Size = New System.Drawing.Size(271, 44)
+        Me.txtCentroCostos.Size = New System.Drawing.Size(170, 23)
         Me.txtCentroCostos.TabIndex = 3
         '
         'txtId
         '
-        Me.txtId.Location = New System.Drawing.Point(12, 0)
+        Me.txtId.Location = New System.Drawing.Point(9, 2)
+        Me.txtId.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.txtId.Name = "txtId"
-        Me.txtId.Size = New System.Drawing.Size(44, 22)
+        Me.txtId.Size = New System.Drawing.Size(34, 20)
         Me.txtId.TabIndex = 4
+        Me.txtId.Visible = False
         '
         'btnCancelar
         '
-        Me.btnCancelar.Location = New System.Drawing.Point(726, 217)
+        Me.btnCancelar.Location = New System.Drawing.Point(668, 177)
+        Me.btnCancelar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnCancelar.Name = "btnCancelar"
-        Me.btnCancelar.Size = New System.Drawing.Size(91, 43)
+        Me.btnCancelar.Size = New System.Drawing.Size(68, 35)
         Me.btnCancelar.TabIndex = 19
         Me.btnCancelar.Text = "Cancelar"
         Me.btnCancelar.UseVisualStyleBackColor = True
         '
         'btnEliminar
         '
-        Me.btnEliminar.Location = New System.Drawing.Point(726, 315)
+        Me.btnEliminar.Location = New System.Drawing.Point(668, 217)
+        Me.btnEliminar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnEliminar.Name = "btnEliminar"
-        Me.btnEliminar.Size = New System.Drawing.Size(91, 43)
+        Me.btnEliminar.Size = New System.Drawing.Size(68, 35)
         Me.btnEliminar.TabIndex = 18
         Me.btnEliminar.Text = "Eliminar"
         Me.btnEliminar.UseVisualStyleBackColor = True
         '
         'btnEditar
         '
-        Me.btnEditar.Location = New System.Drawing.Point(726, 266)
+        Me.btnEditar.Location = New System.Drawing.Point(668, 137)
+        Me.btnEditar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnEditar.Name = "btnEditar"
-        Me.btnEditar.Size = New System.Drawing.Size(91, 43)
+        Me.btnEditar.Size = New System.Drawing.Size(68, 35)
         Me.btnEditar.TabIndex = 17
         Me.btnEditar.Text = "Editar"
         Me.btnEditar.UseVisualStyleBackColor = True
         '
         'btnGuardar
         '
-        Me.btnGuardar.Location = New System.Drawing.Point(726, 168)
+        Me.btnGuardar.Location = New System.Drawing.Point(668, 98)
+        Me.btnGuardar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnGuardar.Name = "btnGuardar"
-        Me.btnGuardar.Size = New System.Drawing.Size(91, 43)
+        Me.btnGuardar.Size = New System.Drawing.Size(68, 35)
         Me.btnGuardar.TabIndex = 16
         Me.btnGuardar.Text = "Guardar"
         Me.btnGuardar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
@@ -131,9 +141,10 @@ Partial Class frmDepartamento
         '
         'btnNuevo
         '
-        Me.btnNuevo.Location = New System.Drawing.Point(726, 119)
+        Me.btnNuevo.Location = New System.Drawing.Point(668, 19)
+        Me.btnNuevo.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnNuevo.Name = "btnNuevo"
-        Me.btnNuevo.Size = New System.Drawing.Size(91, 43)
+        Me.btnNuevo.Size = New System.Drawing.Size(68, 35)
         Me.btnNuevo.TabIndex = 15
         Me.btnNuevo.Text = "Nuevo"
         Me.btnNuevo.UseVisualStyleBackColor = True
@@ -141,33 +152,38 @@ Partial Class frmDepartamento
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(15, 87)
+        Me.Label3.Location = New System.Drawing.Point(11, 71)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(79, 16)
+        Me.Label3.Size = New System.Drawing.Size(63, 13)
         Me.Label3.TabIndex = 20
         Me.Label3.Text = "Descripción"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(425, 87)
+        Me.Label4.Location = New System.Drawing.Point(407, 71)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(84, 16)
+        Me.Label4.Size = New System.Drawing.Size(67, 13)
         Me.Label4.TabIndex = 21
         Me.Label4.Text = "Jefe de Área"
         '
         'txtDescripcion
         '
-        Me.txtDescripcion.Location = New System.Drawing.Point(133, 84)
+        Me.txtDescripcion.Location = New System.Drawing.Point(102, 63)
+        Me.txtDescripcion.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtDescripcion.Multiline = True
         Me.txtDescripcion.Name = "txtDescripcion"
-        Me.txtDescripcion.Size = New System.Drawing.Size(271, 22)
+        Me.txtDescripcion.Size = New System.Drawing.Size(301, 30)
         Me.txtDescripcion.TabIndex = 22
         '
         'txtJefeArea
         '
-        Me.txtJefeArea.Location = New System.Drawing.Point(546, 84)
+        Me.txtJefeArea.Location = New System.Drawing.Point(486, 68)
+        Me.txtJefeArea.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.txtJefeArea.Name = "txtJefeArea"
-        Me.txtJefeArea.Size = New System.Drawing.Size(271, 22)
+        Me.txtJefeArea.Size = New System.Drawing.Size(178, 20)
         Me.txtJefeArea.TabIndex = 23
         '
         'Panel1
@@ -177,27 +193,30 @@ Partial Class frmDepartamento
         Me.Panel1.Controls.Add(Me.Label6)
         Me.Panel1.Controls.Add(Me.txtBuscar)
         Me.Panel1.Controls.Add(Me.dgvDepartamento)
-        Me.Panel1.Location = New System.Drawing.Point(18, 123)
+        Me.Panel1.Location = New System.Drawing.Point(14, 100)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(702, 297)
+        Me.Panel1.Size = New System.Drawing.Size(650, 241)
         Me.Panel1.TabIndex = 24
         '
         'lblTotal
         '
         Me.lblTotal.AutoSize = True
         Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTotal.Location = New System.Drawing.Point(156, 276)
+        Me.lblTotal.Location = New System.Drawing.Point(117, 224)
+        Me.lblTotal.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblTotal.Name = "lblTotal"
-        Me.lblTotal.Size = New System.Drawing.Size(43, 16)
+        Me.lblTotal.Size = New System.Drawing.Size(36, 13)
         Me.lblTotal.TabIndex = 4
         Me.lblTotal.Text = "Total"
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(16, 274)
+        Me.Label7.Location = New System.Drawing.Point(12, 223)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(144, 16)
+        Me.Label7.Size = New System.Drawing.Size(113, 13)
         Me.Label7.TabIndex = 3
         Me.Label7.Text = "Usuarios Registrados: "
         '
@@ -205,17 +224,19 @@ Partial Class frmDepartamento
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Lucida Sans", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(314, 23)
+        Me.Label6.Location = New System.Drawing.Point(338, 19)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(64, 19)
+        Me.Label6.Size = New System.Drawing.Size(51, 16)
         Me.Label6.TabIndex = 2
         Me.Label6.Text = "Buscar"
         '
         'txtBuscar
         '
-        Me.txtBuscar.Location = New System.Drawing.Point(384, 20)
+        Me.txtBuscar.Location = New System.Drawing.Point(393, 15)
+        Me.txtBuscar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.txtBuscar.Name = "txtBuscar"
-        Me.txtBuscar.Size = New System.Drawing.Size(298, 22)
+        Me.txtBuscar.Size = New System.Drawing.Size(224, 20)
         Me.txtBuscar.TabIndex = 1
         '
         'dgvDepartamento
@@ -224,15 +245,26 @@ Partial Class frmDepartamento
         Me.dgvDepartamento.AllowUserToDeleteRows = False
         Me.dgvDepartamento.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.dgvDepartamento.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.id_departamento, Me.nombre_area_departamento, Me.descripcion, Me.centro_de_costos, Me.nombre_jefe_area})
-        Me.dgvDepartamento.Location = New System.Drawing.Point(20, 45)
+        Me.dgvDepartamento.Location = New System.Drawing.Point(14, 37)
+        Me.dgvDepartamento.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.dgvDepartamento.Name = "dgvDepartamento"
         Me.dgvDepartamento.ReadOnly = True
         Me.dgvDepartamento.RowHeadersVisible = False
         Me.dgvDepartamento.RowHeadersWidth = 51
         Me.dgvDepartamento.RowTemplate.Height = 24
         Me.dgvDepartamento.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
-        Me.dgvDepartamento.Size = New System.Drawing.Size(662, 226)
+        Me.dgvDepartamento.Size = New System.Drawing.Size(627, 184)
         Me.dgvDepartamento.TabIndex = 0
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(668, 58)
+        Me.btnBuscar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(68, 35)
+        Me.btnBuscar.TabIndex = 25
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
         '
         'id_departamento
         '
@@ -251,7 +283,7 @@ Partial Class frmDepartamento
         Me.nombre_area_departamento.MinimumWidth = 6
         Me.nombre_area_departamento.Name = "nombre_area_departamento"
         Me.nombre_area_departamento.ReadOnly = True
-        Me.nombre_area_departamento.Width = 230
+        Me.nombre_area_departamento.Width = 170
         '
         'descripcion
         '
@@ -260,7 +292,7 @@ Partial Class frmDepartamento
         Me.descripcion.MinimumWidth = 6
         Me.descripcion.Name = "descripcion"
         Me.descripcion.ReadOnly = True
-        Me.descripcion.Width = 120
+        Me.descripcion.Width = 230
         '
         'centro_de_costos
         '
@@ -269,7 +301,7 @@ Partial Class frmDepartamento
         Me.centro_de_costos.MinimumWidth = 6
         Me.centro_de_costos.Name = "centro_de_costos"
         Me.centro_de_costos.ReadOnly = True
-        Me.centro_de_costos.Width = 180
+        Me.centro_de_costos.Width = 120
         '
         'nombre_jefe_area
         '
@@ -278,28 +310,19 @@ Partial Class frmDepartamento
         Me.nombre_jefe_area.MinimumWidth = 6
         Me.nombre_jefe_area.Name = "nombre_jefe_area"
         Me.nombre_jefe_area.ReadOnly = True
-        Me.nombre_jefe_area.Width = 140
-        '
-        'btnBuscar
-        '
-        Me.btnBuscar.Location = New System.Drawing.Point(726, 364)
-        Me.btnBuscar.Name = "btnBuscar"
-        Me.btnBuscar.Size = New System.Drawing.Size(91, 43)
-        Me.btnBuscar.TabIndex = 25
-        Me.btnBuscar.Text = "Buscar"
-        Me.btnBuscar.UseVisualStyleBackColor = True
         '
         'frmDepartamento
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ControlDark
-        Me.ClientSize = New System.Drawing.Size(861, 437)
+        Me.ClientSize = New System.Drawing.Size(749, 355)
         Me.Controls.Add(Me.btnBuscar)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.txtJefeArea)
         Me.Controls.Add(Me.txtDescripcion)
         Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtCentroCostos)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.btnCancelar)
         Me.Controls.Add(Me.btnEliminar)
@@ -307,14 +330,14 @@ Partial Class frmDepartamento
         Me.Controls.Add(Me.btnGuardar)
         Me.Controls.Add(Me.btnNuevo)
         Me.Controls.Add(Me.txtId)
-        Me.Controls.Add(Me.txtCentroCostos)
         Me.Controls.Add(Me.txtDepartamento)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.MaximizeBox = False
         Me.Name = "frmDepartamento"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "frmDepartamento"
+        Me.Text = "Gestion de Areas y departamentos"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         CType(Me.dgvDepartamento, System.ComponentModel.ISupportInitialize).EndInit()
--- Formularios/frmDepartamento.vb
+++ Formularios/frmDepartamento.vb
@@ -3,6 +3,7 @@
 Public Class frmDepartamento
     Private Sub frmDepartamento_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         DesactivarControlles()
+        TamañoReal()
     End Sub
 
     Sub DesactivarControlles()
@@ -46,15 +47,187 @@ Public Class frmDepartamento
     End Sub
 
     Sub TamañoReal()
-        Width = 662
-        Height = 169
+        Width = 765
+        Height = 170
+        Panel1.Visible = False
     End Sub
+
+    Sub TamañoRealBuscar()
+        Width = 765
+        Height = 380
+        Panel1.Visible = True
+        txtBuscar.Focus()
+        txtBuscar.BackColor = Color.AliceBlue
+
+    End Sub
+
+    Sub InsertarDepartamento()
+        Dim sql As String
+        Dim id As Integer
+
+        If MsgBox("Guardar nuevo Departamento o Area", vbQuestion + vbYesNo, "Sistema de inventario") = vbNo Then
+            Exit Sub
+        End If
+
+        adaptador = New SqlDataAdapter("SELECT * FROM t_departamento_area WHERE nombre_area_departamento = '" & txtDepartamento.Text & "'", obtenerConexion)
+        tabla.Clear()
+        adaptador.Fill(tabla)
+        If tabla.Rows.Count > 0 Then
+            txtDepartamento.Text = tabla.Rows(0).Item("nombre_area_departamento")
+            MsgBox("El registro ya exixte en la base de datos", vbInformation, "Sistema Inventario")
+            Exit Sub
+        End If
+
+        If txtDepartamento.Text = "" Or txtDescripcion.Text = "" Or txtCentroCostos.Text = "" Or txtJefeArea.Text = "" Then
+            MsgBox("Existen Campos vacios", vbInformation, "Sistema Inventario")
+            Exit Sub
+        Else
+            sql = "INSERT INTO [dbo].[t_departamento_area] ([nombre_area_departamento],[centro_de_costos],[nombre_jefe_area],[descripcion]) VALUES ('" & txtDepartamento.Text & "','" & txtCentroCostos.Text & "','" & txtJefeArea.Text & "','" & txtDescripcion.Text & "')"
+            Dim conect As New SqlConnection(obtenerConexion)
+            conect.Open()
+            Using comando As New SqlCommand(sql, conect)
+                id = comando.ExecuteScalar()
+            End Using
+            conect.Close()
+            MsgBox("Registro realizxado con exito", vbInformation, "sistema Inventario")
+        End If
+
+    End Sub
+
+    Sub LlenarDatos()
+        Dim sql As String
+        sql = "SELECT * FROM t_departamento_area"
+        Try
+            Dim tabla As New DataTable
+            adaptador = New SqlDataAdapter(sql, obtenerConexion)
+            adaptador.Fill(tabla)
+            dgvDepartamento.DataSource = tabla
+            lblTotal.Text = tabla.Rows.Count
+
+        Catch ex As Exception
+            MsgBox("Se ha detectado el siguiente error" + ex.ToString + " Sistema Inventario")
+        End Try
+    End Sub
+
+    Sub Editar()
+        Dim id As Integer
+        If txtId.Text = "" Then
+            MsgBox("Existen campos vacios", vbInformation, "Sistema de inventario")
+        Else
+            Dim sql As String
+            sql = "UPDATE t_departamento_area SET nombre_area_departamento = '" & txtDepartamento.Text & "', centro_de_costos = '" & txtCentroCostos.Text & "', 
+                   nombre_jefe_area = '" & txtJefeArea.Text & "', descripcion = '" & txtDescripcion.Text & "'
+                   WHERE id_departamento = '" & txtId.Text & "'"
+            Dim conect As New SqlConnection(obtenerConexion)
+            conect.Open()
+            Using comando As New SqlCommand(sql, conect)
+                id = comando.ExecuteScalar()
+            End Using
+            conect.Close()
+            MsgBox("Registro editado exitosamnete.", vbInformation, "Sistema de inventario")
+            LimpiarControles()
+        End If
+    End Sub
+
+    Sub Eliminar()
+        Dim id As Integer
+        If txtId.Text = "" Then
+            MsgBox("Existen campos vacios", vbInformation, "Sistema de inventario")
+        Else
+            If MsgBox("Esta seguro que desea eliminar el registro: " + Trim(txtDepartamento.Text) + " !!", vbQuestion + vbYesNo, "Sistema de inventario") = vbNo Then
+                LimpiarControles()
+                Exit Sub
+            Else
+                Dim sql As String
+                sql = "DELETE FROM t_departamento_area WHERE id_departamento = " & Trim(txtId.Text)
+                Dim conect As New SqlConnection(obtenerConexion)
+                conect.Open()
+                Using comando As New SqlCommand(sql, conect)
+                    id = comando.ExecuteScalar()
+
+                End Using
+                conect.Close()
+                MsgBox("Registro Eliminado exitosamnete.", vbInformation, "Sistema de inventario")
+                LimpiarControles()
+            End If
+        End If
+    End Sub
+
     Private Sub btnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
         ActivarControlles()
     End Sub
 
     Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
-        DesactivarControlles()
+
+        InsertarDepartamento()
+        LlenarDatos()
+        Call DesactivarControlles()
         LimpiarControles()
+
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        TamañoRealBuscar()
+        btnCancelar.Enabled = True
+        btnBuscar.Enabled = False
+        LlenarDatos()
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        TamañoReal()
+        LimpiarControles()
+        DesactivarControlles()
+        Panel1.Visible = False
+        btnCancelar.Enabled = False
+        btnBuscar.Enabled = True
+    End Sub
+
+    Private Sub txtBuscar_TextChanged(sender As Object, e As EventArgs) Handles txtBuscar.TextChanged
+        If txtBuscar.Text = "" Then
+            LlenarDatos()
+        End If
+
+        adaptador = New SqlDataAdapter("SELECT * FROM t_departamento_area WHERE nombre_area_departamento LIKE '%" & txtBuscar.Text & "%'", obtenerConexion)
+
+        tabla.Clear()
+        adaptador.Fill(tabla)
+        If tabla.Rows.Count > 0 Then
+            dgvDepartamento.DataSource = tabla
+            lblTotal.Text = tabla.Rows.Count
+        Else
+            dgvDepartamento.DataSource = ""
+        End If
+    End Sub
+
+    Private Sub dgvDepartamento_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvDepartamento.CellDoubleClick
+        On Error Resume Next
+        txtId.Text = CStr(dgvDepartamento.Item("id_departamento", dgvDepartamento.CurrentCell.RowIndex).Value)
+        txtDepartamento.Text = CStr(dgvDepartamento.Item("nombre_area_departamento", dgvDepartamento.CurrentCell.RowIndex).Value)
+        txtCentroCostos.Text = CStr(dgvDepartamento.Item("centro_de_costos", dgvDepartamento.CurrentCell.RowIndex).Value)
+        txtJefeArea.Text = CStr(dgvDepartamento.Item("nombre_jefe_area", dgvDepartamento.CurrentCell.RowIndex).Value)
+        txtDescripcion.Text = CStr(dgvDepartamento.Item("descripcion", dgvDepartamento.CurrentCell.RowIndex).Value)
+
+        btnCancelar.Enabled = True
+        btnEditar.Enabled = True
+        btnEliminar.Enabled = True
+        btnNuevo.Enabled = True
+
+        txtDepartamento.Enabled = True
+        txtDescripcion.Enabled = True
+        txtJefeArea.Enabled = True
+
+        txtDepartamento.Focus()
+    End Sub
+
+    Private Sub btnEditar_Click(sender As Object, e As EventArgs) Handles btnEditar.Click
+        Editar()
+        DesactivarControlles()
+        LlenarDatos()
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Eliminar()
+        DesactivarControlles()
+        LlenarDatos()
     End Sub
 End Class
